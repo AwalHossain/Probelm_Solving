4d31556263d6b6f15698609f94acdfe4de7d6649
AwalHossain <waqin21@gmail.com>
2022-06-28 21:20:12 +0600

    plus one solution added

---
+++ July/PlusOne/plusOne-66.js
@@ -0,0 +1,21 @@
+ var plusOne = function(nums) {
+    let arr = nums;
+ for (let i = arr.length -1; i>= 0; i--){
+        if(arr[i] < 9){
+            
+            arr[i] = arr[i] + 1;
+            return arr;
+        }else{
+            arr[i] = 0;
+        
+        }
+
+    }
+    console.log(arr,"ham")
+            arr.unshift(1)
+        console.log(arr,"leter")
+
+        return arr;
+};   
+
+console.log(plusOne([9,9,9]));
