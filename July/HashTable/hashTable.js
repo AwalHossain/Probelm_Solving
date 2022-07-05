6de10c495ca86b28ec81e033b32f3dd91836da0e
AwalHossain <waqin21@gmail.com>
2022-07-05 15:43:02 +0600

    hash table collision

--- July/HashTable/hashTable.js
+++ July/HashTable/hashTable.js
@@ -37,10 +37,37 @@ class HashTable {
     return undefined;
   }
 
+
+  keys() {
+    if (!this.data.length) {
+      return undefined
+    }
+    let result = []
+    // loop through all the elements
+    for (let i = 0; i < this.data.length; i++) {
+        // if it's not an empty memory cell
+        if (this.data[i] && this.data[i].length) {
+          // but also loop through all the potential collisions
+          if (this.data.length > 1) {
+            for (let j = 0; j < this.data[i].length; j++) {
+              result.push(this.data[i][j][0])
+            }
+          } else {
+            result.push(this.data[i][0])
+          } 
+        }
+    }
+    return result; 
+  }
+
 }
 
 const myHashTable = new HashTable(5);
 // myHashTable._hash('va')
-myHashTable.set('gram', 53)
 myHashTable.set('ram', 53)
-console.log(myHashTable.get("ram"),"Kim")
+
+myHashTable.set('gram', 53)
+myHashTable.set('mina', 93)
+myHashTable.set('viar', 33)
+
+console.log(myHashTable.keys(),"mai")
