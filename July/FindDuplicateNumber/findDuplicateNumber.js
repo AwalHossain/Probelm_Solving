fd712a448f04afb9509d948f297abaf46d9835d7
AwalHossain <waqin21@gmail.com>
2022-07-06 07:11:51 +0600

    find duplicate number solved with brutefore😐

---
+++ July/FindDuplicateNumber/findDuplicateNumber.js
@@ -0,0 +1,20 @@
+
+function firstRecurringCharacter(input){
+
+
+    for(let i=0; i<input.length; i++){
+
+        for(let j=i+1; j<input.length; j++){
+            if(input[i] == input[j]){
+
+            // console.log(input[i],"got");
+            return input[i]
+                
+            }
+        }
+    }
+
+}
+
+
+console.log(firstRecurringCharacter([2,5,5,2,3,5,1,2,4]));
