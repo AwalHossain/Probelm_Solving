cd91bac1c1d1d2bc10a23050c094fe96c1c8f6cd
AwalHossain <waqin21@gmail.com>
2022-07-14 16:10:46 +0600

    longest prefix problem solved

---
+++ July/secondWeek/LongestCommonPrefix/LogestCommonPrefix-14.js
@@ -0,0 +1,17 @@
+
+
+var longestCommonPrefix = function(strs) {
+    // strs = strs.join("");
+
+    let prefix = strs[0];
+    for(let i=1; i<strs.length; i++){
+        while(strs[i].indexOf(prefix) !== 0){
+          prefix = prefix.substring(0, prefix.length-1)
+        }
+    }
+
+    return prefix;
+};
+
+
+console.log(longestCommonPrefix(["flower","flow","flight"]));
