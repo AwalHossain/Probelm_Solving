3b58d4fb7529e31666f4950200a8d3706882a0bd
AwalHossain <waqin21@gmail.com>
2022-07-17 20:15:53 +0600

    refurbrushing yesterday's problem

--- june3rdWeek/House_Robber/houseRobber.js
+++ june3rdWeek/House_Robber/houseRobber.js
@@ -1,14 +1,21 @@
 
 var rob = function (nums){
     let length = nums.length;
+    // intializing even and odd number
     let even = 0;
     let odd = 0;
+
+    // for odd number starting from zero indices and added two plus every time
     for(let i =0; i<length; i+=2){
       odd += nums[i]
     }
+
+    // for even number starting from one indices and added two plus every time
     for(let i=1; i<length; i+=2){
       even += nums[i]
     }
+
+    // comparing number and return maximum number
     return Math.max(odd, even);
 }
 
