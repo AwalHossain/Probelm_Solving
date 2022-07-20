efcfc8031027d06d4c40cdb0191f7d98e97419b2
AwalHossain <waqin21@gmail.com>
2022-07-20 23:12:11 +0600

    linked list append and prepend is done

--- July/ThirdWeek/LinkedList/firstfLinkedList.js
+++ July/ThirdWeek/LinkedList/firstfLinkedList.js
@@ -26,10 +26,41 @@ class LinkedList {
         this.tail = this.head;
         this.length = 1;
     }
+
+    append(value){
+        const newNode = {
+            value : value,
+            next: null
+        };
+
+        this.tail.next = newNode;
+
+        this.tail = newNode;
+
+        this.length++;
+        // return this;
+    }
+
+    prepend(value){
+        const newPre = {
+            value: value,
+            next: null
+        }
+        newPre.next = this.head;
+        this.head = newPre;
+       
+    }
 }
 
 
 
 const myFirstLinkedList = new LinkedList(10);
+
+
+// myFirstLinkedList.append(5)
+myFirstLinkedList.append(15)
+// myFirstLinkedList.prepend(22)
+myFirstLinkedList.prepend(32)
+// myFirstLinkedList.prepend(12)
 
 console.log(myFirstLinkedList);
