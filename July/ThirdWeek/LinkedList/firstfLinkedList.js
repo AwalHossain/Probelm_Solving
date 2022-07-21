7d5a2484214301d38a06e17d6b756a95ae57f029
AwalHossain <waqin21@gmail.com>
2022-07-21 14:27:12 +0600

    linked-list modified & insert method added

--- July/ThirdWeek/LinkedList/firstfLinkedList.js
+++ July/ThirdWeek/LinkedList/firstfLinkedList.js
@@ -40,11 +40,10 @@ class LinkedList {
 
     prepend(value) {
         const newNode = new Node(value);
-        this.tail.next = newNode;
-        this.tail = newNode;
-
+        
+        newNode.next = this.head;
+        this.head = newNode;
         this.length++;
-
         return this;
 
     }
@@ -52,10 +51,62 @@ class LinkedList {
     //    append method is going to add item at very begining of the linked-list
 
     append(value) {
-
+        console.log(value,'oval');
         const newNode = new Node(value)
-        newNode.next = this.head;
-        this.head = newNode;
+        
+        this.tail.next = newNode;
+        this.tail = newNode;
+        this.length++;
+
+        return this;
+    }
+
+    printList(){
+            let arr = [];
+        let currentNode = this.head;
+            console.log(arr,'oo');
+            while(currentNode !== null){
+                
+                arr.push(currentNode.value);
+               currentNode =  currentNode.next;
+            }
+
+            return arr;
+    }
+
+    insert(index,value){
+        
+     if(index >= this.length) {
+      console.log('yes',index)
+      return this.prepend(value);
+    }
+
+  const newNode = {
+      value: value,
+      next: null
+    }
+    const leader = this.traverseToIndex(index-1);
+    const holdingPointer = leader.next;
+    leader.next = newNode;
+    newNode.next = holdingPointer;
+
+    this.length++;
+    return this.printList();
+}
+
+      traverseToIndex(index) {
+        let coutner = 0;
+
+        let currentNode = this.head;
+
+        while(coutner !== index){
+            
+            currentNode = currentNode.next;
+
+            coutner++
+        }
+
+        return currentNode;
     }
 
 }
@@ -67,8 +118,11 @@ const myFirstLinkedList = new LinkedList(10);
 
 // myFirstLinkedList.append(5)
 myFirstLinkedList.append(15)
-myFirstLinkedList.prepend(22)
+myFirstLinkedList.prepend(22);
+// console.log(myFirstLinkedList.insert(2, 32));
+myFirstLinkedList.insert(2,12)
+
 // myFirstLinkedList.prepend(32)
 // myFirstLinkedList.prepend(12)
 
-console.log(myFirstLinkedList);
+console.log(myFirstLinkedList.printList());
