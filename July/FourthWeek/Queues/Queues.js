465d61f5126bc866724fa33360c92bae9e383b11
AwalHossain <waqin21@gmail.com>
2022-07-23 20:17:42 +0600

    queues data structure added

---
+++ July/FourthWeek/Queues/Queues.js
@@ -0,0 +1,68 @@
+
+
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor(){
+    this.first = null;
+    this.last = null;
+    this.length = 0;
+  }
+  peek() {
+
+    return this.last;
+  }
+  enqueue(value){
+    const newNode = new Node(value);
+
+    if(this.length === 0){
+
+        this.first = newNode;
+        this.last = newNode
+    }
+    else{
+
+        this.last.next = newNode;
+        this.last = newNode
+
+    }
+    this.length++;
+
+    return this;
+
+  }
+  dequeue(){
+
+  if (!this.first) {
+      return null;
+    }
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    const holdingPointer = this.first;
+    this.first = this.first.next;
+    this.length--;
+    return this;
+
+  }
+  //isEmpty;
+}
+
+const myQueue = new Queue();
+
+myQueue.enqueue("joy")
+// myQueue.enqueue("matt")
+// myQueue.enqueue("pavel")
+myQueue.dequeue();
+console.log(myQueue);
+//Joy
+//Matt
+//Pavel
+//Samir
+
