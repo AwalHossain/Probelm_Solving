a00940c1087f76b87c26c568a4d31c85bbfe80fa
AwalHossain <waqin21@gmail.com>
2022-07-24 20:06:17 +0600

    lookup method added in tree data structure🚀

--- July/FourthWeek/Trees/Trees.js
+++ July/FourthWeek/Trees/Trees.js
@@ -41,6 +41,21 @@ class BinarySearchTree {
   }
   lookup(value) {
     //Code here
+
+      if (!this.root) {
+      return false;
+    }
+    let currentNode = this.root;
+    while(currentNode){
+      if(value < currentNode.value){
+        currentNode = currentNode.left;
+      } else if(value > currentNode.value){
+        currentNode = currentNode.right;
+      } else if (currentNode.value === value) {
+        return currentNode;
+      }
+    }
+    return null
   }
   // remove
 
