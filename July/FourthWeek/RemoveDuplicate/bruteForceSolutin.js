33ecd1d716938359fe1e87565fcfc39bbea4615d
AwalHossain <waqin21@gmail.com>
2022-07-25 14:18:42 +0600

    duplicate sorted array solution modified with differen object hashmap'

--- July/FourthWeek/RemoveDuplicate/bruteForceSolutin.js
+++ July/FourthWeek/RemoveDuplicate/bruteForceSolutin.js
@@ -2,18 +2,20 @@
 
 var removeDuplicates = function(nums) {
     
-    let set  = new Set();
+    let set  = {};
     for(let i=0; i<nums.length; i++){
-
-        if(set.has(nums[i])){
-            delete set.delete(nums[i-1]);
+let check = set[nums[i]];
+        if(check){
+            delete nums[i-1];
         }
 
-        set.add(nums[i]);
+        set[nums[i]] = i;
     }
 
-    return set;
+    nums = Object.keys(set);
+
+    return nums;
 };
 
 
-console.log(removeDuplicates([1,1,2]));
+console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4]));
