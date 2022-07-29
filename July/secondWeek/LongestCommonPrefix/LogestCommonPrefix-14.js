337ff96f1bdfe2e85cd931d2db963981f6c6cae8
Awal hossain <waqin21@gmail.com>
2022-07-29 21:33:47 +0600

    longest substring modified 👍

--- July/secondWeek/LongestCommonPrefix/LogestCommonPrefix-14.js
+++ July/secondWeek/LongestCommonPrefix/LogestCommonPrefix-14.js
@@ -4,8 +4,9 @@ var longestCommonPrefix = function(strs) {
     
     let prefix = strs[0];
     for(let i=1; i<strs.length; i++){
-        while(strs[i].indexOf(prefix) !== 0){
-          prefix = prefix.substring(0, prefix.length-1)
+      while(strs[i].indexOf(prefix) !== 0){
+        prefix = prefix.substring(0, prefix.length-1)
+        console.log(strs[i]);
         }
     }
 
