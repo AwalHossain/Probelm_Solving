0bd118fd1e4c2ae0c87ab3d1be28bdf726bc5108
Awal hossain <waqin21@gmail.com>
2022-08-01 17:39:38 +0600

    linked-list reversed solution modified

--- July/FourthWeek/Reverse-LinkedList/reversLinkList-206.js
+++ July/FourthWeek/Reverse-LinkedList/reversLinkList-206.js
@@ -1,20 +1,44 @@
+/*
+NOTE: The beginning portion builds our test case linked list. Scroll down to the section titled Our Solution for the code solution!
+ */
 
-
-
-var reverseList = function(head) { 
-   let current = head;
-   let prev = null;
-   
-   while(current){
-        let next = current.next;
-        current.next = prev;
-        prev = current;
-
-        current = next;
+class ListNode {
+   constructor(val, next = null) {
+     this.val = val;
+     this.next = next;
    }
-
+ }
+ // ---- Generate our linked list ----
+ const linkedList = [5, 4, 3, 2, 1].reduce((acc, val) => new ListNode(val, acc), null);
+ 
+ // ---- Generate our linked list ----
+ 
+ const printList = (head) => {
+   if(!head) {
+     return;
+   }
+ 
+   console.log(head.val);
+   printList(head.next);
+ }
+ 
+ // --------- Our solution -----------
+ 
+ var reverseList = function(head) {
+   let prev = null;
+   let current = head;
+   
+   while(current) {
+     let nextTemp = current.next;
+     current.next = prev;
+     prev = current;
+     current = nextTemp;
+   }
+   
    return prev;
-}
-
-
-console.log(reverseList([1,2,3,4,5]));
+ };
+ 
+ printList(linkedList);
+ console.log('after reverse')
+ printList(reverseList(linkedList))
+ 
