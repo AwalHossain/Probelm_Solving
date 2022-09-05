975e47f7f398764d972a049b828baf924453e91d
Awal hossain <waqin21@gmail.com>
2022-09-05 19:04:32 +0600

    problem-26 another soultion has added 🥳🥳�

---
+++ Two-Pointers/problem-2-26.js
@@ -0,0 +1,22 @@
+var removeDuplicates = function(nums) {
+
+    let set = new Set();
+
+    for(let i=0; i<nums.length; i++){
+
+        let val = nums[i];
+
+        if(set.has(val)){
+            set.delete(i)
+        }
+
+        set.add(val);
+
+    }
+
+    return nums = [...set];
+
+}
+
+
+console.log(removeDuplicates([1,1,3,3,5,6,6,6,9,9]));
