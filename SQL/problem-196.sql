e702627f249ed405c32dea1e9c7a967275325f5d
Awal hossain <waqin21@gmail.com>
2022-09-07 20:18:06 +0600

    delet duplicate value by inner join table itself 😍😍

---
+++ SQL/problem-196.sql
@@ -0,0 +1,5 @@
+# Please write a DELETE statement and DO NOT write a SELECT statement
+# Write your MySQL query statement below
+
+delete p1.* from  person as p1 inner join person p2
+on p1.email = p2.email and p1.id>p2.id  
