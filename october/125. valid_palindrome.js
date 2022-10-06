b54542e0b5b75ad4f6b723fa92494ad652e7cfcc
Awal hossain <waqin21@gmail.com>
2022-10-06 15:46:00 +0600

    valid palindrome string solved 🔥🔥

---
+++ october/125. valid_palindrome.js
@@ -0,0 +1,16 @@
+var isPalindrome = function(s) {
+    let t =   s.toLowerCase().replace(/[^0-9a-z]/gi, '').split('');;
+      let i=0, j=t.length-1;
+      while(i<j){
+          [t[i],t[j]] = [t[j], t[i]]
+          
+          i++;
+          j--;
+      }
+      
+      console.log(t,"check", s.toLowerCase().replace(/[^0-9a-z]/gi, '').split(''))
+      return  s.toLowerCase().replace(/[^0-9a-z]/gi, '')==t.join("");
+  };
+
+
+  console.log(isPalindrome("ab@a"));
