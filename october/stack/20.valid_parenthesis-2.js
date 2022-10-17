db64e509fe7b5afbc236cdbae2b40791463b9f82
Awal hossain <waqin21@gmail.com>
2022-10-17 08:04:07 +0600

    valid parenthesis second solution 🔥

---
+++ october/stack/20.valid_parenthesis-2.js
@@ -0,0 +1,35 @@
+var isValid = function(s) {
+
+    let stack = [];
+
+    for(const bracket of s.split("")){
+
+        const isParenthesis = bracket === "(";
+        if(isParenthesis) stack.push(")");
+
+        const isCurlyBrace = bracket === "{";
+        if(isCurlyBrace) stack.push("}");
+
+        const isSquareBracket = bracket === "[";
+        if(isSquareBracket) stack.push("]");
+
+        const isOpenBracket = isParenthesis || isCurlyBrace || isSquareBracket;
+        if (isOpenBracket) continue;
+        
+        console.log("checking");
+        const isEmpty = !stack.length;
+        const isWrongPair = stack.pop() !== bracket;
+
+        const isInvalid = isEmpty || isWrongPair;
+            console.log(isEmpty, isWrongPair);
+        if(isInvalid) return false
+    
+    }
+    // console.log(stack);
+    if(stack.length === 0) return true;
+}
+
+
+
+
+console.log(isValid("()[]{}"));
