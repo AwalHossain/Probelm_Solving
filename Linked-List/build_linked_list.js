2c561d43d116a824cbc73b6945df3979cd775e20
Awal hossain <waqin21@gmail.com>
2022-10-18 15:05:22 +0600

    linked list converts into array  🔥🔥

--- Linked-List/build_linked_list.js
+++ Linked-List/build_linked_list.js
@@ -6,43 +6,54 @@ class Node {
 }
 
 
-
 class LinkedList{
-   
     constructor(value){
         this.head = new Node(value, null);
         this.tail = this.head;
-        this.length = 1;
+        this.length=1;
     }
 
     append(value){
-        let current = this.head;
-        const newNode = new Node(value,null);
-  
-        this.tail.next = newNode;
-        this.tail = newNode
-        this.length++;
+        const newNode = new Node(value, null);
 
+        this.tail.next = newNode;
+        this.tail = newNode;
+
+        this.length++;
         return this;
     }
 
     prepend(value){
-        const newNode = new Node(value,null);
+        const newNode = new Node(value, null);
 
-        console.log(newNode,"pre");
-        
         newNode.next = this.head;
         this.head = newNode;
 
         this.length++;
         return this;
     }
+
+    printList(value){
+        const arr = [];
+        let currentNode = this.head;
+
+        while(currentNode !== null){
+            arr.push(currentNode.value);
+
+            currentNode = currentNode.next;
+        }
+
+        return arr;
+    }
+
     
 
-
 }
+
+
+
 const myLinkedList = new LinkedList(10);
 myLinkedList.append(20)
 myLinkedList.prepend(30)
-// myLinkedList.prepend(40)
-console.log(myLinkedList)
+myLinkedList.prepend(40)
+console.log(myLinkedList.printList())
