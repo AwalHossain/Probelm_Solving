b285665016dda6d4ad34487cae276e34b8551e9e
Awal hossain <waqin21@gmail.com>
2022-10-26 09:08:42 +0600

    new solution for remove duplicate 🔥

--- october/array/26. Remove_duplicat.js
+++ october/array/26. Remove_duplicat.js
@@ -1,15 +1,23 @@
-var removeDuplicates = function(nums) {
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var removeDuplicates = function(nums) {
 
-    for(let i=0;i<nums.length; i++){
-        if(nums[i] == nums[i+1]){
-           nums.splice(i,1);
-            console.log(nums,"inside")
-            i--;
+
+    
+    let left =0;
+    for(let right=0;right<nums.length; right++){
+        if(nums[right] !== nums[right+1]){
+           nums[left] = nums[right];
+            left++;
+            
         }
-        
     }
- 
- }
+    
+   return left;
+    
+}
 
 
- console.log(removeDuplicates([1,1,2]));
+console.log(removeDuplicates(1,1,3));
