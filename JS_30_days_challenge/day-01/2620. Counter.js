7f1f2402a7d334202218322d2dfd73f4f5af3aef
AwalHossain <waqin21@gmail.com>
2023-08-30 07:32:39 +0600

    Add comments explaing the problem 🪶🪶

--- JS_30_days_challenge/day-01/2620. Counter.js
+++ JS_30_days_challenge/day-01/2620. Counter.js
@@ -3,16 +3,23 @@
  * @param {number} n
  * @return {Function} counter
  */
-var createCounter = function(n) {
-                    
-        return function counter(){
-                    return   n++;
-                }
-        
- };
- 
- 
-     const counter = createCounter(10)
-     counter() // 10
-    counter() // 11
-    counter() // 12
+var createCounter = function (n) {
+
+    return function counter() {
+        return n++;
+    }
+
+};
+
+/**
+ *  n++ is post increment, so it will return the value of n before incrementing
+ * ++n is pre increment, so it will return the value of n after incrementing
+ * n+1 is just adding 1 to n, but not incrementing n
+ * +n is just converting n to a number
+ * --n is pre decrement, so it will return the value of n after decrementing
+ */
+
+const counter = createCounter(10)
+counter() // 10
+counter() // 11
+counter() // 12
