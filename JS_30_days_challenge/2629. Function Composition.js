46cb67553218f0ec87bc557b8d4a5fa9fc2d96b3
AwalHossain <waqin21@gmail.com>
2023-09-01 07:43:34 +0600

    function composition 🤜

---
+++ JS_30_days_challenge/2629. Function Composition.js
@@ -0,0 +1,37 @@
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+
+
+/*
+* here in this problem, we have to return a function that will take a number as an argument
+* and return the result of the composition of the given functions
+* if the given array is empty, return a function that will take a number as an argument and return the number
+* else return a function that will take a number as an argument and return the result of the composition of the given functions
+* to do that, we have to loop through the given array from the last element to the first element
+* for each element in the array, call the element with the result of the previous function call
+* return the result
+*/
+
+var compose = function(functions) {
+
+    if(functions.length == 0){
+        return function(x){
+            return x
+        }
+    }
+            return function(x){
+                    let result =x;
+                    for(let i = functions.length -1; i>=0; i--){
+                        result = functions[i](result)
+                    }
+                    return result;
+                }
+    
+    };
+    
+    /**
+     * const fn = compose([x => x + 1, x => 2 * x])
+     * fn(4) // 9
+     */
