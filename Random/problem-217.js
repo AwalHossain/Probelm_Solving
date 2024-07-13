ba724f0e9ae983d6743d03d0289d11227825a8df
Awalhossain <waqin21@gmail.com>
2024-07-13 10:11:33 +0600

    refactor: Optimize containsDuplicate function for improved performance

--- Random/problem-217.js
+++ Random/problem-217.js
@@ -1,20 +1,41 @@
-var containsDuplicate = function(nums) {
-    let obj = {};
-       
-       for(let i=0; i<nums.length; i++){
-           
-           let check = obj[nums[i]];
-           
-           if(check >=0){
-               return true;
-           }else{
-               
-               obj[nums[i]] = i;
-           }
-       }
-       
-       return false
-   };
+var containsDuplicate = function (nums) {
+  let obj = {};
 
+  for (let i = 0; i < nums.length; i++) {
+    let check = obj[nums[i]];
 
-   console.log(containsDuplicate([2,4,4,5,3]));
+    if (check >= 0) {
+      return true;
+    } else {
+      obj[nums[i]] = i;
+    }
+  }
+
+  return false;
+};
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function (nums) {
+  let obj = {};
+
+  for (let i of nums) {
+    if (obj[i]) {
+      return true;
+    } else {
+      obj[i] = 1;
+    }
+  }
+  return false;
+};
+
+let check = new Set();
+for (let i of nums) {
+  if (check.has(i)) return true;
+  check.add(i);
+}
+return false;
+
+console.log(containsDuplicate([2, 4, 4, 5, 3]));
