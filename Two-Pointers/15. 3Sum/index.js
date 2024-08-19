9633482db581e18ee978f2a2c0e9befbbc7fbd95
Awalhossain <waqin21@gmail.com>
2024-08-19 13:51:36 +0600

    refactor: Optimize threeSum function for improved performance and readability

--- Two-Pointers/15. 3Sum/index.js
+++ Two-Pointers/15. 3Sum/index.js
@@ -1,29 +1,26 @@
-var threeSum = function (nums) {
-    const res = [];
-    nums.sort((a, b) => a - b);
-
-    for (let i = 0; i < nums.length; i++) {
-        let a = nums[i];
-        if (a > 0) break;
-        if (i > 0 && a == nums[i - 1]) continue;
-
-        let l = i + 1; let r = nums.length - 1;
-        while (l < r) {
-            let threeSum = a + nums[l] + nums[r];
-
-            if (threeSum > 0) {
-                r--;
-            } else if (threeSum < 0) {
-                l++;
-            } else {
-                res.push([a, nums[l], nums[r]]);
-                l++;
-
-                while (nums[l] === nums[l - 1] && l < r) {
-                    l++;
-                }
-            }
-        }
+function threeSum(nums) {
+  nums.sort((a, b) => a - b);
+  let res = [];
+  for (let i = 0; i < nums.length; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) {
+      continue;
     }
-    return res;
-};
+    let l = i + 1,
+      r = nums.length - 1;
+    while (l < r) {
+      let sum = nums[i] + nums[l] + nums[r];
+      if (sum > 0) {
+        r--;
+      } else if (sum < 0) {
+        l++;
+      } else {
+        res.push([nums[i], nums[l], nums[r]]);
+        l++;
+        while (nums[l] == nums[l - 1] && l < r) {
+          l++;
+        }
+      }
+    }
+  }
+  return res;
+}
