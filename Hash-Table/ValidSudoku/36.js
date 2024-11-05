6e8019f24bb4cae4a9ec2afa0ac11fc9e325fb7f
Awalhossain <waqin21@gmail.com>
2024-11-05 14:56:46 +0600

    refactor: Simplify isValidSudoku function and update test cases

--- Hash-Table/ValidSudoku/36.js
+++ Hash-Table/ValidSudoku/36.js
@@ -1,47 +1,26 @@
-/**
- * @param {character[][]} board
- * @return {boolean}
- */
 var isValidSudoku = function (board) {
-  let rows = Array.from({ length: 9 }, () => new Set());
-  console.log(rows, "hem");
-  const column = Array.from({ length: 9 }, () => new Set());
-  const squares = Array.from({ length: 9 }, () => new Set()); // key = (r / 3) * 3 + c / 3
-
+  let row = Array.from({ length: 9 }, () => new Set());
+  console.log(row, "");
   for (let r in board) {
-    for (let c in board) {
+    for (let c in board[r]) {
       let cell = board[r][c];
+      console.log(r, "man", Math.floor(r / 3) * 3 + Math.floor(c / 3));
       if (cell === ".") {
         continue;
       }
-      const squareIndex = Math.floor(r / 3) * 3 + Math.floor(c / 3);
-
-      if (
-        rows[r].has(cell) ||
-        column[c].has(cell) ||
-        squares[squareIndex].has(cell)
-      ) {
-        return false;
-      }
-
-      rows[r].add(cell);
-      column[c].add(cell);
-      squares[squareIndex].add(cell);
     }
   }
-  return true;
 };
-
-const board = [
-  ["5", "3", ".", ".", "7", ".", ".", ".", "."],
-  ["6", ".", ".", "1", "9", "5", ".", ".", "."],
-  [".", "9", "8", ".", ".", ".", ".", "6", "."],
-  ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
-  ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+let test = [
+  ["1", "2", ".", ".", "3", ".", ".", ".", "."],
+  ["4", ".", ".", "5", ".", ".", ".", ".", "."],
+  [".", "9", "8", ".", ".", ".", ".", ".", "3"],
+  ["5", ".", ".", ".", "6", ".", ".", ".", "4"],
+  [".", ".", ".", "8", ".", "3", ".", ".", "5"],
   ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
-  [".", "6", ".", ".", ".", ".", "2", "8", "."],
-  [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+  [".", ".", ".", ".", ".", ".", "2", ".", "."],
+  [".", ".", ".", "4", "1", "9", ".", ".", "8"],
   [".", ".", ".", ".", "8", ".", ".", "7", "9"],
 ];
 
-console.log(isValidSudoku(board));
+console.log(isValidSudoku(test));
